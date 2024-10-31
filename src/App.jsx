17057d835fff7255fc05aa001644a7beaf3c0e15
Liad Yosef <liady@users.noreply.github.com>
2024-10-31 17:12:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import * from 'fs'
+import * as fs from 'fs'
 import './App.css'
 
 function App() {
